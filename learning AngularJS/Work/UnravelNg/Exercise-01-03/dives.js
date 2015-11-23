ac134f144db7540213082306dfd226930baf2415
Jessie <truelj@hotmail.com>
2015-11-22 23:55:31 -0800

    Add a new directory to start learning AngularJS; Repeating the html content by using Jquery and AngularJS;

---
+++ learning AngularJS/Work/UnravelNg/Exercise-01-03/dives.js
@@ -0,0 +1,20 @@
+/**
+ * Created by lenovo on 2015/11/22.
+ */
+var dives=[{
+    site: 'Abu Gotta Ramada',
+    location:'Hurghada, Egypt',
+    depth: 72,
+    time: 54
+},{
+    site: 'Ponte Mahoon',
+    location:'Maehbourg, Mauritius',
+    depth:54,
+    time:38
+},{
+    site:'Monlnar Cave',
+        location:'Budapest, Hungary',
+        depth:98,
+        time:62
+
+}];
