08aea36c42032874e42d1a85bb1fa321e8943f04
Jessie <Truelj@hotmail.com>
2016-08-14 16:49:13 -0700

    Using Js mouse events to create a painting board

--- Java script projects/project2- Painting board/Painting.js
+++ Java script projects/project2- Painting board/Painting.js
@@ -11,31 +11,99 @@ whenReady(function(){
     var background_color = "yellow";
     //The default painting color would be red
     var painting_color = "red";
-
+    var list = document.getElementById("painting-board");
+    //Get a NodeList containing all <li> elements in document
+    var list_items = document.getElementsByTagName("li");
+    var current_item;
+    var move = false;
+    var option = 5;//default option
+    //declare an array of options
+    var options = [{rows:10,columns:10},{rows:20, columns:20}, {rows:25, columns:25},
+                    {rows:50, columns:50}, {rows:100, columns:100}];
     //define the onclick event handler
-    function onclick_item(event){
+    function changeColor(){
+        console.log("change color");
         //set the background of the <li> element
-        var item = event.target;
-        console.log(item);
-        if(!item.colorChanged){
-            item.style.background = painting_color;
-            item.colorChanged = true;
+
+        if(!current_item.colorChanged){
+            current_item.style.background = painting_color;
+            current_item.colorChanged = true;
         }else{
-            item.style.background = background_color;
-            item.colorChanged = false;
+            if(!move)
+                current_item.style.background = background_color;
+                current_item.colorChanged = false;
         }
 
     }
 
-    //Get a NodeList containing all <li> elements in document
-    //Register the onclick event handler on each <li> element
 
-    var list_items = document.getElementsByTagName("li");
+    function downHandler(event){ console.log("drag");
+        //set/reset the current item
+        current_item = event.target;
+        //change the color of current cell
+        changeColor();
+        //register mouse move event handlers on every item
+        for(var i = 0; i < list_items.length; i++){
+            list_items[i].addEventListener("mousemove", moveHandler, false);
+        }
+        //Register capturing event handlers on the document
+        //document.addEventListener("mousemove", moveHandler, true);
+        document.addEventListener("mouseup", upHandler,true);
+        //stop event propagation
+        event.stopPropagation();
+        //prevent any default action
+        event.preventDefault();
+    }
+
+    function moveHandler(event){ console.log("move");
+        move = true;
+        if(event.target !== current_item){
+            current_item = event.target;
+            changeColor();
+        }
+        //stop event propagation
+        event.stopPropagation();
+        //prevent any default action
+        event.preventDefault();
+
+    }
+    function upHandler(event){ console.log("up");
+        console.log("event target: "+ event.target);
+        for(var i = 0; i < list_items.length; i++)
+        {
+            list_items[i].removeEventListener("mousemove", moveHandler,false);
+        }
+        move = false;
+        //document.removeEventListener("mousemove", moveHandler,true);
+        document.removeEventListener("mouseup", upHandler, true);
+        //stop event propagation
+        event.stopPropagation();
+        //prevent any default action
+        event.preventDefault();
+    }
+
+    function create(){
+        console.log("create");
+        //remove current <li> elements in <ul>
+        for(var i = 0; i < list_items.length; i++){
+            list.removeChild(list_items[i]);
+        }
+        //append a new number of <li> elements in <ul>
+        var total = options[option-1].rows * options[option-1].columns;
+        for(var i = 0; i < total; i++){
+            var item = document.createElement("li");
+            item.className = "option" + option;
+            list.appendChild(item);
+        }
+    }
+    //create the painting board, with rows and columns default to be 10
+    create();
+
+    //Register mouse down event handler on each <li> element
     for(var i = 0; i < list_items.length; i++){
-        list_items[i].onclick = onclick_item;
-        //list_items[i].addEventListener("click", onclick_item, false);
+        list_items[i].addEventListener("mousedown", downHandler,false);
         console.log("hello");
-        // add a property to the <li> element
+        // add a property named "colorChanged" to the <li> element.
         list_items[i].colorChanged = false;
     }
 });
